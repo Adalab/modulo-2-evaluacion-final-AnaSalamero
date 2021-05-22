4fd39019d77a880fb13cddc26b55a051fb16c9f8
Ana Salamero <asalamerosanz@gmail.com>
2021-05-22 09:17:00 +0200

    introduced global variable instead of data parameter

--- js/01-global.js
+++ js/01-global.js
@@ -3,6 +3,7 @@
 const movieUlList = document.querySelector('.js-movielist');
 const buttonElement = document.querySelector('.js-btn');
 const inputElement = document.querySelector('.js-input');
+let allMovies = [];
 
 //const palette = data.palettes[0]
 
@@ -18,33 +19,35 @@ function getInfo() {
   fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
+      allMovies = data;
+      console.log(allMovies);
+      renderMovies();
       //console.log(data);
       //console.log(data[0].show.name);
       //console.log(data[0].show.image.medium);
-      for (let i = 0; i < data.length; i++) {
-        console.log(data[i].show);
-        const dataShow = data[i].show;
-        //const dataShowImage = dataShow.image
-        console.log(dataShow);
-        console.log(dataShow.image);
-        // console.log(dataShow.image.medium);
-        if (dataShow.image === null) {
-          movieUlList.innerHTML += `
-            <li class="movie_list-item">
-            <h2 class="movie_title">${dataShow.name}</h2>
-            <img src="https://via.placeholder.com/150"/>
-             </li>`;
-        } else {
-          movieUlList.innerHTML += `
-        <li class="movie_list-item">
-        <h2 class="movie_title">${dataShow.name}</h2>
-        <img src="${dataShow.image.medium}"/>
-         </li>`;
-        }
-      }
     });
 }
 
-// function paintMovies() {
-
-// }
+function renderMovies() {
+  for (let i = 0; i < allMovies.length; i++) {
+    //console.log(data[i].show);
+    const dataShow = allMovies[i].show;
+    //const dataShowImage = dataShow.image
+    console.log(dataShow);
+    console.log(dataShow.image);
+    // console.log(dataShow.image.medium);
+    if (dataShow.image === null) {
+      movieUlList.innerHTML += `
+        <li class="movie_list-item">
+        <h2 class="movie_title">${dataShow.name}</h2>
+        <img src="https://via.placeholder.com/150"/>
+         </li>`;
+    } else {
+      movieUlList.innerHTML += `
+    <li class="movie_list-item">
+    <h2 class="movie_title">${dataShow.name}</h2>
+    <img src="${dataShow.image.medium}"/>
+     </li>`;
+    }
+  }
+}
